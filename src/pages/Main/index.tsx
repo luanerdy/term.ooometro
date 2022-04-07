94527e879f9f772201cfdff046c5c101ac14c99b
luanerdy <luan.vini1@gmail.com>
2022-04-06 21:56:14 -0300

    bugfix: hide keyboard in words suggestions

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -9,20 +9,30 @@ import { PageStyles } from "./styles";
 import { handleSetActiveLetter } from "./utils/keyboard";
 
 const Main = () => {
-
   const { words, setWords } = useContext(WordsContext);
   const [showResults, setShowResults] = useState<boolean>(false);
 
-  const keyboardWrite = async (type: string, newLetter: string = '') => {
+  const keyboardWrite = async (type: string, newLetter: string = "") => {
     setWords(await handleSetActiveLetter(words, type, newLetter));
   };
 
   return (
     <PageStyles>
       <Header />
-      {words.length > 1 && <ButtonResults showResults={showResults} setShowResults={setShowResults} />}
-      {showResults ? <Words /> : <Termo />}
-      <Keyboard setActiveLetter={keyboardWrite} />
+      {words.length > 1 && (
+        <ButtonResults
+          showResults={showResults}
+          setShowResults={setShowResults}
+        />
+      )}
+      {showResults ? (
+        <Words />
+      ) : (
+        <>
+          <Termo />
+          <Keyboard setActiveLetter={keyboardWrite} />
+        </>
+      )}
     </PageStyles>
   );
 };
